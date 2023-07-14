fa3b5211d322a46e900f9cbc78b9ed28547634b6
Chris Martin <ch.martin@gmail.com>
2023-07-14 12:25:22 -0600

    nix cleanup and update

--- nix/default.nix
+++ nix/default.nix
@@ -3,45 +3,39 @@
 let
   inherit (pkgs.lib) fold composeExtensions concatMap attrValues;
 
-  hls = pkgs.haskell-language-server.override {
-    supportedGhcVersions = [ "96" ];
-  };
-
   combineOverrides = old:
     fold composeExtensions (old.overrides or (_: _: { }));
 
-  testConfigurations =
+  makeTestConfiguration = { ghc ? pkgs.haskellPackages, overrides ? new: old: { } }:
     let
-      makeTestConfiguration = { ghcVersion, overrides ? new: old: { } }:
-        let
-          inherit (pkgs.haskell.lib) dontCheck packageSourceOverrides;
-        in
-        (pkgs.haskell.packages.${ghcVersion}.override (old: {
-          overrides =
-            combineOverrides old [
-              (packageSourceOverrides { integer-types = ../integer-types; })
-              overrides
-            ];
-        })).integer-types;
+      inherit (pkgs.haskell.lib) dontCheck packageSourceOverrides;
     in
-    rec {
-      ghc-9-2 = makeTestConfiguration {
-        ghcVersion = "ghc92";
-      };
-      ghc-9-4 = makeTestConfiguration {
-        ghcVersion = "ghc94";
-      };
-      ghc-9-6 = makeTestConfiguration {
-        ghcVersion = "ghc96";
-        overrides = new: old: {
-          # x = new.callPackage ./haskell/x.nix { };
-        };
-      };
-      all = pkgs.symlinkJoin {
-        name = "integer-types-tests";
-        paths = [ ghc-9-2 ghc-9-4 ghc-9-6 ];
+    (ghc.override (old: {
+      overrides =
+        combineOverrides old [
+          (packageSourceOverrides { integer-types = ../integer-types; })
+          overrides
+        ];
+    })).integer-types;
+
+  testConfigurations = rec {
+    ghc-9-2 = makeTestConfiguration {
+      ghc = pkgs.haskell.packages.ghc92;
+    };
+    ghc-9-4 = makeTestConfiguration {
+      ghc = pkgs.haskell.packages.ghc94;
+    };
+    ghc-9-6 = makeTestConfiguration {
+      ghc = pkgs.haskell.packages.ghc96;
+      overrides = new: old: {
+        # x = new.callPackage ./haskell/x.nix { };
       };
     };
+    all = pkgs.symlinkJoin {
+      name = "integer-types-tests";
+      paths = [ ghc-9-2 ghc-9-4 ghc-9-6 ];
+    };
+  };
 
 in
 {
@@ -49,9 +43,11 @@ in
   packages = { inherit testConfigurations; };
 
   devShells.default = pkgs.mkShell {
-    inputsFrom = [ testConfigurations.ghc-9-6.env ];
+    inputsFrom = [
+      (makeTestConfiguration { }).env
+    ];
     buildInputs = [
-      hls
+      pkgs.haskell-language-server
       pkgs.cabal-install
     ];
   };
